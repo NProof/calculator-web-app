fbbf388069bde714956a3d9ae02dd74cf67f0a2e
NProof <endles00000@gmail.com>
2020-03-31 10:37:55 +0800

    d0.0.1

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 document.addEventListener("keyup", function(e) {
-  console.log(`[KeyBoard Event : UP - ${e.code}]`);
+    console.log(`[KeyBoard Event : UP - ${e.code}]`);
 });
 
 $("botton").click(function() {
-  console.log(`Button click.`);
+    console.log(`Button click.`);
 });
 
 let expression = "";
-let display = document.querySelector("#display");
+let current = 0;
+let display = document.querySelector("#display").children[current];
 
 document.addEventListener("keydown", function(e) {
-  let code = e.code;
-  // alert(`{ ${code}, ${key}, ${keyCode}, ${char}}`);
-  if (code === "Enter" || (code === "Equal" && !e.shiftKey)) {
-    try {
-				let result = math.evaluate(expression);
-    		alert(result);
-			}
-			catch(e) {
-				alert(expression.concat(" is invalid."));
-			}
-    expression = "";
-  } else if (code === "ShiftLeft" || code === "ShiftRight") {
-  } else if (code === "Backspace") {
-    expression = expression.slice(0, -1);
-  } else {
-    expression += e.key;
-    console.log("exp : " + expression);
-  }
-  display.value = expression;
+    let code = e.code;
+    // alert(`{ ${code}, ${key}, ${keyCode}, ${char}}`);
+    if (code === "Enter" || (code === "Equal" && !e.shiftKey)) {
+        try {
+            let result = math.evaluate(expression);
+            alert(result);
+        } catch (event) {
+            alert(expression.concat(" is invalid."));
+        }
+        expression = "";
+    } else if (code === "ShiftLeft" || code === "ShiftRight") {} else if (code === "Backspace") {
+        expression = expression.slice(0, -1);
+    } else {
+        expression += e.key;
+        try {
+            display.innerHTML = "$$".concat(math.parse(expression).toTex()).concat("$$");
+            MathJax.typeset();
+        } catch (event) {
+//             alert(" toTex() ");
+        }
+    }
+    display.value = expression;
 });
